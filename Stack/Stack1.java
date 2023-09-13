e6b95cf40c97b3455a1949a3672730a6bd8fe275
pratiksutar841 <pratiksutar841@gmail.com>
2023-09-13 22:10:34 +0530

    Impliment-Stack-Using-ArrayList

--- Stack/Stack1.java
+++ Stack/Stack1.java
@@ -1,5 +1,68 @@
+//Implimentation of Stack using ArrayList
 package Data_Structure_and_Algorithms.Stack;
+import java.util.*;
+public class Stack1 
+{
+     static class stack
+     {
+           static ArrayList<Integer>list=new ArrayList<>(null);
 
-public class Stack1 {
-          
+           public static boolean isEmpty()
+           {
+                    return list.size()==0;
+           }
+
+           //push
+           public static void push(int data)
+           {
+                    
+                    list.add(data);
+           }
+
+           //pop
+           public static  int pop()
+           {
+                    if (isEmpty()) {
+                              return -1;
+                    }
+                    int top = list.get(list.size()-1);
+                    list.remove(list.size()-1);
+                    return top;
+           }
+
+           //peek
+           public static int peek()
+           {
+                     if (isEmpty()) {
+                              return -1;
+                    }
+                    return list.get(list.size()-1);
+           }
+     }     
+  public static void main(String[] args) 
+  {
+    Stack s = new Stack<>();
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+    s.push(6);
+    
+    while (!s.isEmpty()) 
+    {
+          System.out.println(s.peek());;
+          s.pop();
+    }
+  }          
 }
+/*
+ output
+ 6
+ 5
+ 4
+ 3
+ 2
+ 1
+ */
